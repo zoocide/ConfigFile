b1eae0538b10d5019073089efbc4d88f704db038
Alexander Smirnov <zoocide@gmail.com>
2019-12-21 01:03:03 +0300

    tmp

--- t/performance_experiment.pl
+++ t/performance_experiment.pl
@@ -61,9 +61,72 @@ my @tests = (
 );
 EOS
 
-for my $size (1, 4, 16, 128, 512) {
-    print 'strings length = ', length $test_str_part, " * $size.\n";
+$test_str_part = <<'EOS';
+var = one two\ \'a \# b 'a string'# comment
+EOS
 
-    $str = $test_str_part x $size;
-    cmpthese -5  => \%map;
+print "$test_str_part\n";
+my @words = text2words($test_str_part);
+print "<$_>\n" for @words;
+print qq."@words"\n.;
+
+# for my $size (1, 4, 16, 128, 512) {
+    # print 'strings length = ', length $test_str_part, " * $size.\n";
+
+    # $str = $test_str_part x $size;
+    # cmpthese -5  => \%map;
+# }
+
+
+sub text2words {
+  my $text = shift;
+  my @ret;
+
+  my $c;
+  my $inside_string = 0;
+  my $spaces_skipped;
+  for my $s (split /\n/, $text) {
+    while (length $s > 0) {
+      if ($inside_string) {
+        if ($s =~ s/^((?:[^\\']|\\.)++)'//) {
+          # string finished
+          $ret[-1] .= $1;
+          normalize_str($ret[-1]);
+          $inside_string = 0;
+        }
+        else {
+          # string unfinished
+          $ret[-1] .= $s;
+          last;
+        }
+      }
+
+      ## outside string ##
+      ## skip spaces and comments ##
+      $spaces_skipped = ($s =~ s/^\s*(?:#.*)?//); #< skip spaces and comments
+      last if length $s == 0;
+
+      ## take next word ##
+      if ($s =~ s/^((?:[^\\'# \t]|\\[\\$' ])++)//) {
+        # word taken
+        push @ret, $1;
+        normalize_str($ret[-1]);
+      }
+      elsif ($s =~ s/^'//) {
+        # string encountered
+        $inside_string = 1;
+        push @ret, '';
+      }
+      else {
+        die "unexpected string '$s' encountered";
+      }
+    }
+  }
+  @ret
+}
+
+sub normalize_str
+{
+  $_[0] =~ s/\\([\\\$' \t])/$1/g;
+  $_[0]
 }
